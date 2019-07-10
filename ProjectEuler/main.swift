d613714b7c54af1ee86ea21a66df9547733249c5
Peter C Huber <peter@hubersitech.com>
2019-07-10 16:13:07 -0400

    Initial Commit

---
+++ ProjectEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler
+//
+//  Created by Peter Huber on 2019-07-10.
+//  Copyright © 2019 Peter Huber. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
