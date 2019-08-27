3307c2589ada016a833c9d7e170f81daedd2e25e
Peter C Huber <peter@hubersitech.com>
2019-08-27 08:55:24 -0400

    Working on 51

--- ProjectEuler/Project_51.swift
+++ ProjectEuler/Project_51.swift
@@ -10,17 +10,56 @@ import Foundation
 
 func Project51()
 {
-    let lolimit = 10000
-    let hilimit = 100000
-    let numPrimePermutations = 7
+    var theSet = [0,1,2,3,4]
     
-    var primes = OptimizedSieveOfEratosthenes(n: hilimit)
     
-    while primes.first! < lolimit
+    
+    // input parameters
+    let numPrimesToFind = 7
+    let minLimit = 10000
+    
+    let maxDigitsToReplace = DigitArray(n: minLimit).count - 1
+    
+    var numPrimes = 0
+    var currentValue = minLimit
+    
+    while numPrimes < numPrimesToFind
     {
-        primes.removeFirst()
+        
+        currentValue += 1
     }
     
     
     
+}
+
+func GetCombinations(nArray:[Int], k:Int) -> [[Int]]
+{
+    var result:[[Int]] = []
+    
+    return result
+}
+
+func DoCombination(
+
+func DoReplace(n:Int, numDigits:Int) -> [Int]
+{
+    let nAsDigits = DigitArray(n: n)
+    
+    let numDigitsToReplace = min(nAsDigits.count - 1, numDigits)
+    
+    return []
+    
+}
+
+func ReplaceDigits(inDigits:[Int], digitIndices:[Int], newDigit:Int) -> Int
+{
+    var digits = inDigits
+    
+    for nextIndex in digitIndices
+    {
+        digits[nextIndex] = newDigit
+    }
+    
+    return DigitsAsInt(digits: digits)
 }
