7858713f0859b3acea6287f87057968c75f9a25c
Peter Huber <peter@huberistech.com>
2019-08-27 13:59:45 -0400

    Still on 51

--- ProjectEuler/Project_51.swift
+++ ProjectEuler/Project_51.swift
@@ -10,21 +10,27 @@ import Foundation
 
 func Project51()
 {
-    var theSet = [0,1,2,3,4]
     
+    // print(DoCombinationsOfArrayIndices(arraySize: 5, k: 3))
     
     
     // input parameters
     let numPrimesToFind = 7
     let minLimit = 10000
     
-    let maxDigitsToReplace = DigitArray(n: minLimit).count - 1
+    // let maxDigitsToReplace = DigitArray(n: minLimit).count - 1
     
     var numPrimes = 0
     var currentValue = minLimit
     
     while numPrimes < numPrimesToFind
     {
+        let maxDigitsToReplace = CountDigits(n: UInt(currentValue)) - 1
+        
+        for i in 1...maxDigitsToReplace
+        {
+            
+        }
         
         currentValue += 1
     }
@@ -33,24 +39,72 @@ func Project51()
     
 }
 
-func GetCombinations(nArray:[Int], k:Int) -> [[Int]]
+func CountDigits(n:UInt) -> Int
 {
-    var result:[[Int]] = []
+    var result = 0
+    
+    var useN = n
+    
+    while useN > 0
+    {
+        result += 1
+        useN /= 10
+    }
     
     return result
 }
 
-func DoCombination(
-
-func DoReplace(n:Int, numDigits:Int) -> [Int]
+func DoCombinationsOfArrayIndices(arraySize:Int, k:Int) -> [[Int]]
 {
-    let nAsDigits = DigitArray(n: n)
+    // This method (and its helper) comes from https://www.geeksforgeeks.org/print-all-possible-combinations-of-r-elements-in-a-given-array-of-size-n/
     
-    let numDigitsToReplace = min(nAsDigits.count - 1, numDigits)
+    // k is defined as in "n Choose k"
     
-    return []
+    // clamp k to the size fo the array
+    let useK = (k > arraySize ? arraySize : k)
     
+    var trivalResult:[Int] = []
+    
+    for i in 0..<arraySize
+    {
+        trivalResult.append(i)
+    }
+    
+    // special case where n == k
+    if arraySize == useK
+    {
+        return [trivalResult]
+    }
+    
+    var result:[[Int]] = []
+    
+    var tempArray:[Int] = Array(repeating: 0, count: k)
+    
+    CombHelper(resultArray: &result, indexArray: trivalResult, currentArray: &tempArray, start: 0, end: arraySize - 1, index: 0, r: k)
+    
+    
+    return result
 }
+
+func CombHelper(resultArray: inout [[Int]], indexArray:[Int], currentArray: inout [Int], start:Int, end:Int, index:Int, r:Int)
+{
+    if index == r
+    {
+        resultArray.append(currentArray)
+        return
+    }
+    
+    var i = start
+    while i <= end && end - i + 1 >= r - index
+    {
+        currentArray[index] = indexArray[i]
+        
+        CombHelper(resultArray: &resultArray, indexArray: indexArray, currentArray: &currentArray, start: i + 1, end: end, index: index + 1, r: r)
+        
+        i += 1
+    }
+}
+
 
 func ReplaceDigits(inDigits:[Int], digitIndices:[Int], newDigit:Int) -> Int
 {
--- ProjectEuler/main.swift
+++ ProjectEuler/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-Project60()
+Project51()
 
 
 
